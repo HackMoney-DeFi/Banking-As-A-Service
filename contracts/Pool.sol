28b5b06e3c2fbbf28c635843efb11d141154eb17
Kaleb Gebrekirstos <ksgebrekirstos98@gmail.com>
2021-06-29 15:49:09 -0700

    Added add and remove functionalities and added unit tests

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -21,21 +21,31 @@ contract Pool is IPool {
     string public name;
 
     /*
-     * List of users who have special roles such as managing funds and voting on loan requests
+     * Map to keep track of users who have special roles such as managing funds and voting on loan requests
      */
-    address[] public admins;
+    mapping(address => bool) public admins;
 
     /*
      * Map to keep track of how much a user has deposited to and withdrawn from the Pool
      */
     mapping(address => uint256) userBalance;
 
+    modifier requireUserIsAdmin(address _address) {
+        require(admins[_address] == true,
+            "User is not authorized to perform operation because they are not an Admin.");
+        _;
+    }
+
+    event AddedAdmin(address _address);
+
+    event RemovedAdmin(address _adddress);
+
     constructor (string memory _name, address[] memory _admins) {
         name = _name;
 
         // Deep copy
         for (uint i = 0; i < _admins.length; i++) {
-            admins.push(_admins[i]);
+            admins[_admins[i]] = true;
         }
     }
 
@@ -47,12 +57,14 @@ contract Pool is IPool {
         //TODO: left intentionally blank
     }
 
-    function addAdmin(address _address) external override {
-        //TODO: left intentionally blank
+    function addAdmin(address _address) external override requireUserIsAdmin(msg.sender) {
+        admins[_address] = true;
+        emit AddedAdmin(_address);
     }
 
-    function removeAdmin(address _address) external override {
-        //TODO: left intentionally blank
+    function removeAdmin(address _address) external override requireUserIsAdmin(msg.sender) {
+        delete admins[_address];
+        emit RemovedAdmin(_address);
     }
 
     function transfer(address to, address from, uint256 _amount) public override {
