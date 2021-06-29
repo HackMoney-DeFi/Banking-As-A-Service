ea64c9a2a88a187207039c2debbec8e705f2a023
Tad <thaddeusgetachew@gmail.com>
2021-06-28 22:32:55 -0400

    add demo tests
    eat

--- test/PoolFactory.js
+++ test/PoolFactory.js
@@ -8,13 +8,37 @@ describe("PoolFactory contract", function () {
 
   beforeEach(async function () {
     poolFactoryContract = await ethers.getContractFactory("PoolFactory");
-    [owner] = await ethers.getSigners();
+    [owner, addy1, addy2] = await ethers.getSigners();
 
     poolFactoryToken = await poolFactoryContract.deploy();
     await poolFactoryToken.deployed();
-
-    await poolFactoryToken.deployed();
   });
 
-  // TODO: Add tests
+  
+  describe("Pool dynamics", function () {
+    // `it` is another Mocha function. This is the one you use to define your
+    // tests. It receives the test name, and a callback function.
+
+    // If the callback function is async, Mocha will `await` it.
+    it("Should create a pool", async function () {
+      //create a pool
+      poolRef = await poolFactoryToken.createPool("tadPool", [addy1.address]);
+      pools = await poolFactoryToken.listPools();
+      expect( pools.length).to.equal(1);
+    });
+
+    it("Pool should have admin var defined", async function () {
+      // Acquire pool contract
+      poolRef = await poolFactoryToken.createPool("tadPool2", [addy2.address]);
+      poolConFac = await ethers.getContractFactory("Pool");
+      poolCon = await poolConFac.attach(addy2.address);
+
+      // just for demo
+      // remove if DS is changed
+      expect(await poolCon.admins.length).to.equal(0);
+      
+    });
+ 
+  });
 });
+
