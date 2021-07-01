ccd2f7efaa27e6f33138d14c9e28852e65918746
= <bktotient@gmail.com>
2021-06-30 23:26:53 -0400

    set pool staking limit

--- test/Pool.js
+++ test/Pool.js
@@ -9,13 +9,38 @@ describe("Pool contract", function () {
     poolFactoryContract = await ethers.getContractFactory("PoolFactory");
     [admin, nonAdmin] = await ethers.getSigners();
 
-    poolFactoryToken = await poolFactoryContract.deploy();
+    libFactory = await ethers.getContractFactory("LibToken");
+    LibToken = await libFactory.deploy("LibToken", "Lib");
+    await LibToken.deployed();
+
+
+    skLibFactorty = await ethers.getContractFactory("StkLibToken")
+    skLibToken = await skLibFactorty.deploy(LibToken.address, "stkLib Token",  "stkLib")
+    await skLibToken.deployed();
+
+
+
+    poolFactoryToken = await poolFactoryContract.deploy(skLibToken.address);
     await poolFactoryToken.deployed();
   });
+  
+
+  async function StkToken(amount) {
+
+    await LibToken.mint(admin.address, amount) 
+    await LibToken.approve(skLibToken.address, amount)
+    expect(await LibToken.allowance(admin.address, skLibToken.address)).to.equal(amount)
+
+    // Stake LibToken
+    await skLibToken.stake(amount)
+    expect(await skLibToken.balanceOf(admin.address)).to.equal(amount)
+    expect(await LibToken.balanceOf(admin.address)).to.equal(0)
+  }
 
   
   describe("Pool functionality", function () {
     it("Admin should be able to add/remove users to/from the admin list", async function () {
+      await StkToken(ethers.BigNumber.from("1000000000000000000000000000"))
       //create a pool and an instance of the contract
       await poolFactoryToken.createPool("TestPool1", [admin.address]);
       const poolAddress = (await poolFactoryToken.listPools())[0];
